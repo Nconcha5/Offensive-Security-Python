f8d3a0b7901b5a707ef63f5f8d329c2798e57b08
Nconcha5 <90735535+Nconcha5@users.noreply.github.com>
2024-06-11 21:50:13 -0500

    Update hash cracker.py

--- hash cracker.py
+++ hash cracker.py
@@ -2,42 +2,39 @@ import hashlib
 from hashlib import md5, sha1, sha256, sha384
 
 def get_hash():
-    Hash = str(input("Enter a valid hash"))
+    Hash = input("Enter a valid hash: ")
     return Hash
 
 def Read_PassFile():
-    File = open("PassFile.txt","r")
-    passwords = [x.strip('\n') for x in File.readlines()]
-    File.close()
+    with open("PassFile.txt", "r") as File:
+        passwords = [x.strip('\n') for x in File.readlines()]
     return passwords
 
 def Hash_Function(password, Hashed_Pass):
-    global Found 
     if md5(password.encode()).hexdigest() == Hashed_Pass:
-        Found = True
-        print("Found {0} : {1} : md5".format(Hashed_Pass, password))
-
+        print(f"Found {Hashed_Pass} : {password} : md5")
+        return True
     elif sha1(password.encode()).hexdigest() == Hashed_Pass:
-        Found = True 
-        print("Found {0} : {1} : sha1".format(Hashed_Pass. password))
-
+        print(f"Found {Hashed_Pass} : {password} : sha1")
+        return True
     elif sha256(password.encode()).hexdigest() == Hashed_Pass:
-        Found = True 
-        print("Found {0} : {1} : 256".format(Hashed_Pass, password))
-
+        print(f"Found {Hashed_Pass} : {password} : sha256")
+        return True
     elif sha384(password.encode()).hexdigest() == Hashed_Pass:
-        Found = True
-        print("Found {0} : {1} : sha384".format(Hashed_Pass. password))   
+        print(f"Found {Hashed_Pass} : {password} : sha384")
+        return True
+    return False
 
-Found = False 
 List = Read_PassFile()
+
 while True:
-    cmd = str(input("$ "))
-    if cmd == "Hash":
+    cmd = input("$ ").strip()
+    if cmd.lower() == "hash":
         Hash = get_hash()
         for password in List:
-            Hash_Function(password, Hash)
-            if Found:
+            if Hash_Function(password, Hash):
                 break
         else:
-         print("Command Unknown")
+            print("Hash not found.")
+    else:
+        print("Command Unknown")
