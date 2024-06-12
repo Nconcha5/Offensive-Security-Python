47cecc8f7d035bfffb97b7a7ee0e18b2d9fa94f7
Nconcha5 <90735535+Nconcha5@users.noreply.github.com>
2024-06-11 22:00:05 -0500

    Update duplicater.py

--- duplicater.py
+++ duplicater.py
@@ -2,19 +2,21 @@ from sys import argv
 import os
 
 folder_name = 'Clone'
-
 count = 0
 
-script = argv
-main_file_name = str(script[0])
+script = sys.argv
+main_file_name = os.path.basename(script[0])
 
-for i in range(10):
-    count += 1
-    temp = folder_name +str(count)
-    os.mkdir(temp)
-    os.system(r"copy " + main_file_name +" {0}".format(temp))
-
+try:
+    for i in range(10):
+        count += 1
+        temp = folder_name + str(count)
+        os.mkdir(temp)
+        shutil.copy(main_file_name, temp)
 except Exception as Error:
-print(Error)
+    print(Error)
+
 count += 10
+
+
 
